44801dc64b78e5609c9709bc42edaea73a3e6e7a
stanislavermohin <ermohin1999@gmail.com>
2023-04-17 17:30:53 +0700

    Add .env, reformat main.py, change test data in inventory.db

--- main.py
+++ main.py
@@ -1,40 +1,14 @@
 import inspect
-from typing import List
-from typing import Type
+import os
+from typing import List, Type
 
 from fastapi import FastAPI, Depends, Form, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from pydantic.fields import ModelField
 from sqlalchemy import create_engine, Column, Integer, String, Float
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-
-def as_form(cls: Type[BaseModel]):
-    new_parameters = []
-
-    for field_name, model_field in cls.__fields__.items():
-        model_field: ModelField  # type: ignore
-
-        new_parameters.append(
-            inspect.Parameter(
-                model_field.alias,
-                inspect.Parameter.POSITIONAL_ONLY,
-                default=Form(...) if model_field.required else Form(model_field.default),
-                annotation=model_field.outer_type_,
-            )
-        )
-
-    async def as_form_func(**data):
-        return cls(**data)
-
-    sig = inspect.signature(as_form_func)
-    sig = sig.replace(parameters=new_parameters)
-    as_form_func.__signature__ = sig  # type: ignore
-    setattr(cls, 'as_form', as_form_func)
-
-    return cls
+from sqlalchemy.orm import sessionmaker, declarative_base
+from uvicorn import run
 
 
 app = FastAPI()
@@ -57,12 +31,46 @@ Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
 
+class BetterBaseModel(BaseModel):
+
+    class Config:
+        orm_mode = True
+
+
 def get_session():
     session = Session()
+
     try:
         yield session
     finally:
         session.close()
+
+
+def as_form(cls: Type[BetterBaseModel]):
+    new_parameters = []
+
+    for field_name, model_field in cls.__fields__.items():
+        model_field: ModelField  # type: ignore
+
+        new_parameters.append(
+            inspect.Parameter(
+                model_field.alias,
+                inspect.Parameter.POSITIONAL_ONLY,
+                default=Form(...) if model_field.required else Form(model_field.default),
+                annotation=model_field.outer_type_,
+            )
+        )
+
+    async def as_form_func(**data):
+        return cls(**data)
+
+    sig = inspect.signature(as_form_func)
+    sig = sig.replace(parameters=new_parameters)
+    as_form_func.__signature__ = sig  # type: ignore
+
+    setattr(cls, 'as_form', as_form_func)
+
+    return cls
 
 
 class Product(Base):
@@ -72,15 +80,6 @@ class Product(Base):
     description = Column(String)
     price = Column(Float)
     quantity = Column(Integer)
-
-    @property
-    def id(self):
-        return self._id
-
-
-class BetterBaseModel(BaseModel):
-    class Config:
-        orm_mode = True
 
 
 @as_form
@@ -101,7 +100,8 @@ class ProductOutput(BetterBaseModel):
 
 
 @app.post("/products/", response_model=ProductOutput)
-def create_product(product: ProductInput = Depends(ProductInput.as_form), session: Session = Depends(get_session)):
+async def create_product(product: ProductInput = Depends(ProductInput.as_form),
+                         session: Session = Depends(get_session)):
     db_product = Product(name=product.name, description=product.description, price=product.price,
                          quantity=product.quantity)
     session.add(db_product)
@@ -111,13 +111,13 @@ def create_product(product: ProductInput = Depends(ProductInput.as_form), sessio
 
 
 @app.get("/products/", response_model=List[ProductOutput])
-def get_products(session: Session = Depends(get_session)):
+async def get_products(session: Session = Depends(get_session)):
     products = session.query(Product).all()
     return products
 
 
 @app.get("/products/{product_id}", response_model=ProductOutput)
-def get_product(product_id: int, session: Session = Depends(get_session)):
+async def get_product(product_id: int, session: Session = Depends(get_session)):
     product = session.query(Product).filter(Product._id == product_id).first()
     if not product:
         raise HTTPException(status_code=404, detail="Product not found")
@@ -125,8 +125,8 @@ def get_product(product_id: int, session: Session = Depends(get_session)):
 
 
 @app.put("/products/{product_id}", response_model=ProductOutput)
-def update_product(product_id: int, product: ProductInput = Depends(ProductInput.as_form),
-                   session: Session = Depends(get_session)):
+async def update_product(product_id: int, product: ProductInput = Depends(ProductInput.as_form),
+                         session: Session = Depends(get_session)):
     db_product = session.query(Product).filter(Product._id == product_id).first()
     if not db_product:
         raise HTTPException(status_code=404, detail="Product not found")
@@ -140,10 +140,16 @@ def update_product(product_id: int, product: ProductInput = Depends(ProductInput
 
 
 @app.delete("/products/{product_id}")
-def delete_product(product_id: int, session: Session = Depends(get_session)):
+async def delete_product(product_id: int, session: Session = Depends(get_session)):
     db_product = session.query(Product).filter(Product._id == product_id).first()
     if not db_product:
         raise HTTPException(status_code=404, detail="Product not found")
     session.delete(db_product)
     session.commit()
     return {"message": "Product deleted successfully"}
+
+
+if __name__ == "__main__":
+    run(app="main:app",
+        host="0.0.0.0",
+        port=int(os.environ["PORT"]))
